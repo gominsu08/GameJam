61c8b1008433b8c4ed9fab311422ec87e60f52b5
rex렉스 <rexgood1005@gmail.com>
2024-07-29 20:56:28 +0900

    fix : Entity Move

---
+++ Assets/01_Script/Chipmunk/MoveRequester.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveRequest{
+    public Entity entity;
+}
+public class MoveRequester : MonoSingleton<MoveRequester>
+{
+    // public static 
+}
