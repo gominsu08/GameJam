5949a3a94c32a6308d614b0d96851fec7161beb6
gominsu08 <minsu080618@gmail.com>
2024-07-29 15:36:00 +0900

    fix : RoundManager

--- Assets/01_Script/Lemon/GameTimer.cs
+++ Assets/01_Script/Lemon/GameTimer.cs
@@ -11,33 +11,46 @@ public class GameTimer : MonoBehaviour
     [SerializeField] TMP_Text timerText;
 
     [SerializeField] private RoundManager roundManager;
+
+    private bool isTileMapCreate;
      
     private void Start()
     {        
         timerText.text = $"Time [ {Mathf.Round(CountDown123)} ]";
+        StageManager.Instance.TileSetCoroutineStart();
+        
     }
 
     private void Update()
     {
         if (isTimeFlow == true)
         {
-            if (Mathf.Round(CountDown123) <= 3)
+            if (Mathf.Round(CountDown123) <= 3 && isTileMapCreate)
             {
                 timerText.text = $"Time [ {Mathf.Round(CountDown123)} ]";
                 CountDown123 += Time.deltaTime;
             }
-            else if (Mathf.Round(CountDown123) > 3)
+            else if (Mathf.Round(CountDown123) > 3 && isTileMapCreate)
             {
+                if (SetTime <= 0) return;
                 timerText.text = $"Time [ {Mathf.Round(SetTime)} ]";
                 SetTime -= Time.deltaTime;
+
             }
         }
 
-        if (SetTime <= 0)
+        if (SetTime <= 0 )
         {
             roundManager.EndRound();
             TimeReset();
+            StageManager.Instance.TileSetCoroutineStart();
+            isTileMapCreate = false;
         }
+    }
+
+    public void TileSet()
+    {
+        isTileMapCreate = true;
     }
 
     public void TimeReset()
