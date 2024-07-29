4dd422a773581a7ec728bf7036169fddf6cbe37d
gominsu08 <minsu080618@gmail.com>
2024-07-29 10:39:48 +0900

    feat : TileDestroy

--- Assets/01_Script/Gwamegi/StageManager.cs
+++ Assets/01_Script/Gwamegi/StageManager.cs
@@ -15,6 +15,23 @@ public class StageManager : MonoBehaviour
     [SerializeField] private int _xMinSize;
     [SerializeField] private int _yMaxSize;
     [SerializeField] private int _yMinSize;
+
+
+    private int _xMaxSizeIn;
+    private int _xMinSizeIn;
+    private int _yMaxSizeIn;
+    private int _yMinSizeIn;
+
+
+    private void Awake()
+    {
+        _xMaxSizeIn = _xMaxSize;
+        _yMaxSizeIn = _yMaxSize;
+        _xMinSizeIn = _xMinSize;
+        _yMinSizeIn = _yMinSize;
+    }
+
+
     private void Update()
     {
         _tileMap.transform.position = _tileTransform;
@@ -22,6 +39,15 @@ public class StageManager : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.V))
         {
             StartCoroutine(TileSet(_xMinSize, _xMaxSize, _yMinSize, _yMaxSize));
+            _xMaxSizeIn = _xMaxSize;
+            _yMaxSizeIn = _yMaxSize;
+            _xMinSizeIn = _xMinSize;
+            _yMinSizeIn = _yMinSize;
+        }
+
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            TileDestroy(_xMinSizeIn, _xMaxSizeIn, _yMinSizeIn, _yMaxSizeIn);
         }
     }
 
@@ -31,18 +57,60 @@ public class StageManager : MonoBehaviour
         StartCoroutine(TileSet(_xMinSize, _xMaxSize, _yMinSize, _yMaxSize));
     }
 
-    private IEnumerator TileSet(int xMin,int xMax, int yMin, int yMax)
+    private IEnumerator TileSet(int xMin, int xMax, int yMin, int yMax)
     {
-
-        for (int i = xMin; i < xMax; i++) 
+        for (int i = xMin; i <= xMax; i++)
         {
 
-            for (int j = yMin; j < yMax; j++)
+            for (int j = yMin; j <= yMax; j++)
             {
                 _tileMap.SetTile(new Vector3Int(i, j), _baseTile);
-                yield return new WaitForSeconds(0.01f);
+                yield return new WaitForSeconds(0.005f);
             }
-        
+
         }
+    }
+
+    public void TileDestroyCoroutineStart()
+    {
+
+    }
+
+
+    private void TileDestroy(int xMin, int xMax, int yMin, int yMax)
+    {
+        if (xMin == xMax) return;
+        if (yMax == yMin) return;
+
+        int xMaxSize = xMax;
+        int xMinSize = xMin;
+        int yMaxSize = yMax;
+        int yMinSize = yMin;
+
+        for (int i = xMinSize; i <= xMaxSize; i++)
+        {
+            _tileMap.SetTile(new Vector3Int(i, yMinSize), null);
+        }
+
+        for (int i = xMinSize; i <= xMaxSize; i++)
+        {
+            _tileMap.SetTile(new Vector3Int(i, yMaxSize), null);
+        }
+
+        for (int i = yMinSize; i <= yMaxSize; i++)
+        {
+            _tileMap.SetTile(new Vector3Int(xMinSize, i), null);
+        }
+
+        for (int i = yMinSize; i <= yMaxSize; i++)
+        {
+            _tileMap.SetTile(new Vector3Int(xMaxSize, i), null);
+        }
+
+        _xMaxSizeIn--;
+        _xMinSizeIn++;
+        _yMaxSizeIn--;
+        _yMinSizeIn++;
+
     }
 }
