e8ab37ade4988f6e68db0ad583bbccfcc6989ca1
gominsu08 <minsu080618@gmail.com>
2024-07-31 16:23:42 +0900

    fix : EnemyList

--- Assets/01_Script/Gwamegi/TutorialManager.cs
+++ Assets/01_Script/Gwamegi/TutorialManager.cs
@@ -4,8 +4,44 @@ using UnityEngine;
 
 public class TutorialManager : MonoBehaviour
 {
-    
 
+    [SerializeField] private GameObject[] _TTPenal;
+    private int _tTIndex;
+
+    [SerializeField] private GameObject _TTStartPenal;
+
+    private bool _isTTStart;
+
+    private void Update()
+    {
+        if (!_isTTStart) return;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            _TTPenal[_tTIndex].SetActive(false);
+            _tTIndex++;
+            if (_TTPenal[_tTIndex] == null)
+            {
+                _isTTStart = false;
+                _tTIndex = 0;
+                _TTStartPenal.SetActive(false);
+                return;
+            }
+            _TTPenal[_tTIndex].SetActive(true);
+        }
+    }
+
+    public void TTUIStart()
+    {
+        _TTPenal[0].SetActive(true);
+        _TTStartPenal.SetActive(true);
+        //foreach (GameObject item in _TTPenal)
+        //{
+        //    if (_TTPenal[_tTIndex] == null) return;
+        //    item.SetActive(false);
+        //}
+        _isTTStart = true;
+    }
 
 
 }
